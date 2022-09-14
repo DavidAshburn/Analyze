2bb3229d20ea61aacdacf158f75cad36acb3f901
DavidAshburn <oneinchwebbing@gmail.com>
2022-09-14 13:13:04 -0700

    basic calculations done

--- app/javascript/controllers/calc_controller.js
+++ app/javascript/controllers/calc_controller.js
@@ -10,9 +10,13 @@ static targets = [
   "excessre",
   "coversqft",
   "autoliability",
+  "umbrellalimit",
+  "numproperties",
+  "totalpropvalue",
+  "investmentvalue",
   "autoexcessre",
-  "autoexcessumbrella",
-  "personalexcessumbrella",
+  "excessumbrella",
+  "totalexcessumbrella",
   "worklife",
   "deathbenefit",
   "cashvalue",
@@ -27,7 +31,48 @@ static targets = [
   }
 
   update() {
-      
+      this.home = this.homeliabilityTarget.value;
+      this.auto = this.autoliabilityTarget.value;
+      this.umb = this.umbrellalimitTarget.value;
+      this.life = this.deathbenefitTarget.value;
+
+
+      if(this.home > 0) {
+        this.excesshome = this.valueTarget.value - this.home;
+        this.excessreTarget.innerText = `$${this.excesshome}`;
+      }
+
+      if(this.footageTarget.value > 0) {
+        this.sqftout = (this.dwellingTarget.value * this.ercTarget.value) / this.footageTarget.value ;
+        this.coversqftTarget.innerText = `$${this.sqftout}`;
+        console.log("sqft")
+      }
+
+      if(this.auto > 0) {
+        this.autoexcess = this.valueTarget.value - this.auto;
+        this.autoexcessreTarget.innerText = `$${this.autoexcess}`;
+      }
+
+      if(this.auto > this.home)
+        this.lower = this.home;
+      else
+        this.lower = this.auto;
+
+      if(this.umb > 0) {
+        this.worstcase = parseInt(this.lower) + parseInt(this.umb);
+        this.worstcase = this.totalpropvalueTarget.value - this.worstcase;
+        this.excessumbrellaTarget.innerText = `$${this.worstcase}`;
+        this.worstcase += parseInt(this.investmentvalueTarget.value);
+        this.totalexcessumbrellaTarget.innerText = `$${this.worstcase}`;
+      }
+
+      if(this.life > 0) {
+        this.yearsleft = this.life / parseFloat(this.currentannualTarget.value);
+        this.yearsincomeTarget.innerText = this.yearsleft;
+
+        this.benefiteachTarget.innerText = `$${this.life / parseFloat(this.dependentsTarget.value)}`
+      }
+
   }
 
 
