62405f8b8b0146f5dacd871daf8840268597867a
DavidAshburn <oneinchwebbing@gmail.com>
2022-09-14 11:56:35 -0700

    wrote and styled index

--- app/javascript/controllers/calc_controller.js
+++ app/javascript/controllers/calc_controller.js
@@ -1,9 +1,34 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-static targets = []
+static targets = [
+  "value",
+  "footage",
+  "dwelling",
+  "erc",
+  "homeliability",
+  "excessre",
+  "coversqft",
+  "autoliability",
+  "autoexcessre",
+  "autoexcessumbrella",
+  "personalexcessumbrella",
+  "worklife",
+  "deathbenefit",
+  "cashvalue",
+  "currentannual",
+  "dependents",
+  "yearsincome",
+  "benefiteach"
+]
 
   connect() {
     
   }
+
+  update() {
+      
+  }
+
+
 }
