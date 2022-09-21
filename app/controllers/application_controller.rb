5de0c17eff54026bb00428b8b2c3b3d348c8c56c
DavidAshburn <oneinchwebbing@gmail.com>
2022-09-21 11:45:39 -0700

    trimmed out leads

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-  home_profile_path
+  exams_home_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :leads
   devise_for :users
   root 'home#index'
   get 'home/profile'
